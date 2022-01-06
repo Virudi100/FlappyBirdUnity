f121b86cd0c1e292538bd8b9fa3baaee21e1312e
LEGEARD Anthony <77632799+Virudi100@users.noreply.github.com>
2022-01-06 15:12:10 +0100

    Upload Project

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gamemanager : MonoBehaviour
+{
+    public GameObject gameoverCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+   public void GameOver ()
+    {
+        //Active le canvas de gameover et freeze le jeu
+        gameoverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void replay()
+    {
+        //recharge la scene
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/Spawnpipe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnpipe : MonoBehaviour
+{
+    public float maxTime = 1;
+    private float timer = 0;
+    public GameObject pipe;
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instancie un nouveau Gameobject de Tuyaux avec une hauteur Y aléatoire
+        GameObject newpipe = Instantiate(pipe);
+        newpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height),0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTime)
+        {
+            /*Instancie un nouveau Gameobject de Tuyaux avec une hauteur Y aléatoire 
+            et utilise le timer pour les instanciers toujours à la meme distances l'un de l'autre */
+
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height),0);
+            Destroy(newpipe, 15);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
